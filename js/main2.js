6afa6c96cf4cf573bf3351af2145ace3c624d00d
Y2Kuwu <jamesmfox5@gmail.com>
2022-10-30 20:43:42 -0700

    working needs culling and additional logic

--- js/main2.js
+++ js/main2.js
@@ -1,8 +1,8 @@
 // buttonChoices.disabled == true;
-let round = 0; //global acknowledged for first round
+let round = 0; //globally acknowledged for first round
 let win; // add this
 let draw; // add this
-let cpuXO; //needed?
+let cpuXO; 
 let human; //needed?
 
 
@@ -41,21 +41,28 @@ class Game{
    
 
   //Recieves data on boxes selected selection == black box/button , selectionR = redBox
-
-  selected(selection, selectionR){
-    console.log('not start')        
-    round++;  
-    if (round % 2 == 0){
-      this.cpu();
-    }                   
-    if (this.redSelect !== '' && this.boxSelect !== '' && !win && !draw)   
-        //if boxes do not contain data and the status of the game is not win or draw return..
-    return
+  cpuSelected(selection, selectionR){
+  
+  if (this.redSelect !== '' && this.boxSelect !== '' && !win && !draw  && round % 2 !== 0)  
+  return
+    
     this.selection = selection; //current equals var current
     this.selectionR = selectionR; //current equals var current
     this.boxSelect = ''; //equals inputted val
     this.redSelect = ''; //equals inputted val
-    
+    console.log(selection, selectionR.id)
+    this.cpuTileSelect();
+  }
+
+  selected(selection, selectionR){            
+    if (this.redSelect !== '' && this.boxSelect !== '' && !win && !draw)   
+        //if boxes do not contain data and the status of the game is not win or draw return..
+    return
+    round++;
+    this.selection = selection; //current equals var current
+    this.selectionR = selectionR; //current equals var current
+    this.boxSelect = ''; //equals inputted val
+    this.redSelect = ''; //equals inputted val
     console.log(selection, selectionR.id)
     }
   
@@ -73,145 +80,172 @@ class Game{
     switch (this.selection , this.selectionR.id){ 
     case 'b1' , 'box1': //both by id 
       boxIsSelected = 'b1', value = 1; 
-      if (round % 2 !== 0){
       this.selectionR.innerHTML = markXO.value //once black box disappears append red box 
-      }
-      if (round % 2 == 0){
-      this.selectionR.innerHTML = cpuXO
-      }
       break;
     case 'b2', 'box2':
       boxIsSelected = 'b2', value = 2;
-      if (round % 2 !== 0){
-      this.selectionR.innerHTML = markXO.value 
-        }
-      if (round % 2 == 0){
-      this.selectionR.innerHTML = cpuXO
-        }
+      this.selectionR.innerHTML = markXO.value
       break;
     case 'b3', 'box3':
       boxIsSelected = 'b3', value = 3;
-      if (round % 2 !== 0){
-        this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
+        this.selectionR.innerHTML = markXO.value
       break;
     case 'b4', 'box4':
       boxIsSelected = 'b4', value = 4;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
     case 'b5', 'box5':
       boxIsSelected = 'b5', value = 5;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
     case 'b6', 'box6':
       boxIsSelected = 'b6', value = 6;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
     case 'b7', 'box7':
       boxIsSelected = 'b7', value = 7;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
     case 'b8', 'box8':
       boxIsSelected = 'b8', value = 8;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
     case 'b9', 'box9':
       boxIsSelected = 'b9', value = 9;
-      if (round % 2 !== 0){
         this.selectionR.innerHTML = markXO.value 
-        }
-        if (round % 2 == 0){
-        this.selectionR.innerHTML = cpuXO
-        }
       break;
   
     default:
       return;
   }
-    this.redBoxIsSelected;  // new black box var
+    this.redBoxIsSelected;  
     this.value = value; //assigned strictly by given value and passed
     this.boxIsSelected = boxIsSelected
     console.log(redBoxIsSelected)
     console.log(markXO.value)
-    this.collect() //calls to collect values and determine player
+    
+    this.collect();
   }
 
-
-    //
-
-    collect(){
-   
+  collect(){
     //let roundNum = round;  //recieved round number given new variable
     //let keyCount = "round" + round;
+    
     let boxValue = this.value; //recieved value number given new variable
     let boxIds = this.boxIsSelected
     // let turnValue = this.chooseXO.value;
     console.log(round)
       //console.log(roundNum)
-    if(round % 2 !== 0){  //checks for even/odd player 1 goes first (human)
+   // if(round % 2 !== 0){  //checks for even/odd player 1 goes first (human)
     roundPoints["player1"].push(boxValue); //obj roundpoints has two arrays, pushes current value selected
     roundPoints["boxId"].push(boxIds) //grabs id for cpu() to ignore
-    console.log(roundPoints)
     console.log(this.boxIsSelected)
     
+    console.log(round)
+    this.cpu();
     }
-   if(round % 2 == 0){
-    roundPoints["player2"].push(boxValue);
-    roundPoints["boxId"].push(boxIds)
-    console.log(roundPoints)
-    }
-  }
   
+  
+    
+  //last for now
+  cpuTileSelect(){
+let boxIsSelected;
+let redBoxIsSelected;
+let value; 
+
+switch (this.selection , this.selectionR.id){ 
+case 'b1' , 'box1': 
+boxIsSelected = 'b1', value = 1; 
+this.selectionR.innerHTML = cpuXO 
+break;
+case 'b2', 'box2':
+boxIsSelected = 'b2', value = 2;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b3', 'box3':
+boxIsSelected = 'b3', value = 3;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b4', 'box4':
+boxIsSelected = 'b4', value = 4;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b5', 'box5':
+boxIsSelected = 'b5', value = 5;
+this.selectionR.innerHTML = cpuXO 
+break;
+case 'b6', 'box6':
+boxIsSelected = 'b6', value = 6;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b7', 'box7':
+boxIsSelected = 'b7', value = 7;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b8', 'box8':
+boxIsSelected = 'b8', value = 8;
+this.selectionR.innerHTML = cpuXO
+break;
+case 'b9', 'box9':
+boxIsSelected = 'b9', value = 9;
+this.selectionR.innerHTML = cpuXO
+break;
+
+default:
+  return;
+}
+this.redBoxIsSelected;  // new black box var
+this.value = value; //assigned strictly by given value and passed
+this.boxIsSelected = boxIsSelected
+console.log(redBoxIsSelected)
+}
+
+    
+        //THIS IS ONE AHEAD OF SCHEDULE
     cpu(){
+      
       //needs to: click or select button or id , be random , not be previously selected
+      
       console.log(cpuXO)
-      
+
       let remainingLen;
-      remainingLen =  boxBtns.length - roundPoints["boxId"].length;
-      
-      
-      roundPoints["boxId"].forEach(randBtn=>{
-      console.log(randBtn)
-      //boxBtns.forEach(buttonList=>{
-        //console.log(buttonList.id)
-      //  for (let buttonList =0; buttonList<boxBtns.length; buttonList++)
-      //  {
-        if(boxBtns.id !== roundPoints["boxId"]){
-          // console.log(randBtn + "fart")
+      console.log(roundPoints["boxId"]) //all 
+      remainingLen = boxBtns.length-round //- roundPoints["boxId"].length;  //-1
+      //roundPoints["boxId"].forEach(randBtn=>{
+        console.log(boxBtns.length) 
+       //console.log(randBtn)
         var cpuChoice = Math.floor(Math.random()*remainingLen);
-         //console.log( boxBtns[buttonList].id)
-         console.log(boxBtns[cpuChoice])
+       // if(boxBtns.id !== roundPoints["boxId"]){
+        //boxBtns.forEach(cpuButton =>{
+        
+        // if(cpuButton !== randBtn){
+        //   console.log(cpuButton)
+        if (boxBtns[cpuChoice] !== roundPoints["boxId"]){
+        console.log(cpuChoice)
+        console.log(boxBtns[cpuChoice])
+        const red = document.getElementById('box' + boxBtns[cpuChoice].value)
+        console.log(red)
+
+        roundPoints["player2"].push(boxBtns[cpuChoice].value); //value
+        roundPoints["boxId"].push(boxBtns[cpuChoice].id)
+        console.log(roundPoints["boxId"])
+        console.log(roundPoints["player2"])
+        boxBtns[cpuChoice].style.opacity = '0';
+        boxBtns[cpuChoice].disabled = true;
+        // this.cpuSelected(boxBtns[cpuChoice].value, boxBtns[cpuChoice].id)
+        this.cpuSelected(boxBtns[cpuChoice].id, red)
+
+        // this.tileSelect(); THINK IT NEEDS TO BE CALLED AROUND HERE still off by one
+       // cpuGo(boxBtns[cpuChoice].id)
         }
-      })
+      //  })
+      
+      
     }
+     // })
+    //  })
+    
+  
          
       
     
@@ -238,7 +272,7 @@ class Game{
     
 
 
-    check(){
+    //check(){
 
     //let p1 = roundPoints[round1.value,round3,round5,round7,round9]
    //   let p2 = playerTwo;
@@ -282,10 +316,9 @@ class Game{
 //   }
 // }
 //     }
-  }
+ // }
+//}
 }
-
-
 
 const x = document.getElementById('xButton');
 const o = document.getElementById('oButton');
@@ -335,37 +368,3 @@ o.addEventListener("click", xbtn=>{
       })
   })
 
-  function cpuGo(){
-    const redBox = document.getElementById('box' + btn.value)
-    game.selected(btn.id, redBox);
-    btn.style.opacity = '0';
-    btn.disabled = true;
-    game.tileSelect();
-    console.log(btn.value)
-  }
-
-  // function humanVComp(digi){
-// return digi;
-// }
-// function terminatorChoice(termChoice, sel) //makeMark)   //add function to listeners
-// {
-    
-//     for(let matchChoice = 0; matchChoice <= termChoice.length; matchChoice++)
-//     {    
-//     var randChoice = Math.floor(Math.random()*termChoice.length);
-
-//     const choice = termChoice[randChoice];
-   
-//     const mark = sel[randChoice];
-//     if (termChoice[randChoice].style.display === "none") {
-//         termChoice[randChoice].display = "block";
-//       } else {
-//         termChoice[randChoice].style.display = "none";
-//         sel[randChoice].innerHTML = humanVComp(digiChoice);
-//         machine.push(mark);
-       
-//         checkScore(machine, human);
-//    return choice, mark; //, mark;
-//       }
-//     }
-// }
